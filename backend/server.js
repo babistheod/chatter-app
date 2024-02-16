581dfa4608db179996c31ef033b8aa2ebba29587
babistheod <babistheod95@gmail.com>
2024-02-17 00:59:11 +0200

    dev completed

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-//import { app, server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 //const __dirname = path.resolve();
@@ -30,7 +29,7 @@ app.use("/api/users", userRoutes);
 //	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 //});
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
